508d30306efe19e0cf09882ca26ed86babe2a741
reema-ibrahim <88351320+reema-ibrahim@users.noreply.github.com>
2021-10-09 16:54:48 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'reema', // Title for your website.
-    tagline: 'hey guys its reema.',
+    tagline: 'hey guys its reema!',
     profilePhoto: 'img/profile.png',
     url: 'https://reema-ibrahim.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
